9cfd9dc7b21bc77adaa170a6fd9eede44bedbfe9
eleanor-miller <eleanormiller08@icloud.com>
2021-12-08 13:52:26 -0500

    useEffect with an empty array as its list of watching variables runs once at start

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 export function App() {
   const [count, setCount] = useState(0)
   // useEffect:
+  // always going to run at least once
   //        function to call
   //                |          array of variables to watch for changes
   //                |           |
@@ -10,9 +11,17 @@ export function App() {
   //                v           v
   //useEffect(theCountChanged, [count])
 
+  useEffect(
+    function () {
+      console.log(`Wow, the count is now ${count}`)
+    },
+    [count]
+  )
+
+  // This gives us a RUN ONCE method when the component first lands on the page!
   useEffect(function () {
-    console.log(`Wow, the count is now ${count}`)
-  })
+    console.log(`This runs once when the component first mounts`)
+  }, [])
 
   function handleClickButton() {
     setCount(count + 1)
